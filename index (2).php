46a7132f9ff56b1b8a450374c4f8dd9ac16c2bc2
maybeabaa <jesusmendoza05032000@gmail.com>
2025-03-02 03:31:22 -0600

    Update index (2).php

--- index (2).php
+++ index (2).php
@@ -12,6 +12,11 @@ if ($conexion->connect_error) {
     die("Conexión no establecida: " . $conexion->connect_error);
 }
 
+// Configurar CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 // Recepción de Métodos
 header("Content-Type: application/json");
 
